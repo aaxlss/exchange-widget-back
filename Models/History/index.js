fac61efed3919016d940ed5977ef7fb9e65525a7
aaxlss <axl9305@gmail.com>
2022-12-16 20:18:57 +0000

    [update] Updating history schema structure

--- Models/History/index.js
+++ Models/History/index.js
@@ -6,6 +6,8 @@ const historySchema = new Schema({
 
   date_time: Date,
   currency_from: String,
+  curreny_from_asset_id: String,
+  curreny_to_asset_id: String,
   amount_1: Number,
   currency_to: String,
   amount_2: Number,
