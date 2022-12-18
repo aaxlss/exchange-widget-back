f898f380151c3c1cd23a981b691fa0f3d1da41e8
aaxlss <axl9305@gmail.com>
2022-12-18 13:35:01 +0000

    [Update] Updating method to recall the api and updating with websockets

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const iconModel = require('./Models/Icons');
 const routes = require('./routes');
 const cors = require('cors');
 const socket = require('./socket');
+const RECALL_API = 205//min
+
 mongoose.Promise = global.Promise;
+
+const millisecons = () => {
+  let millisecons = RECALL_API * 60000;
+  console.info(`Server will call API every ${millisecons} `);
+  return millisecons
+}
 
 const connect = async () => {
   try {
@@ -16,8 +24,8 @@ const connect = async () => {
       .connect('mongodb://admin:p%40ssw0rd@localhost:27017/?authSource=admin', {
         useNewUrlParser: true,
       })
-      // .then((response) => get_assets());
-      // .then(() => get_icons())
+      .then((response) => get_assets())
+      .then(() => get_icons());
     if (connection) {
       console.log('\x1b[32m%s\x1b[0m', 'Database Connected Successfully...');
     }
@@ -26,7 +34,6 @@ const connect = async () => {
     console.log(error);
   }
 };
-
 
 const port = 3001;
 const api_key = 'C4AACEBD-0DB0-48B4-8532-58D528F691B3';
@@ -45,40 +52,74 @@ const get_assets = () => {
   })
     .then((response) => response.json())
     .then((response) => {
+      console.log(response)
       response.forEach((asset) => {
         const asset_model = new assetModel(asset);
-        asset_model.save(function (err, item) {
-          if (err) return console.error(err);
-          // console.log(item + ' saved to bookstore collection.');
-        });
-      });
-    });
-};
-const get_icons = () => {
-  fetch(URL + icons_assets,{
-    method:'GET',
-    headers:{
-      'X-CoinAPI-Key': api_key,
-    }
-  })
-  .then(response => response.json())
-  .then(response => {
-    console.log(response)
-    response.forEach(icon => {
-      const icon_model = new iconModel(icon)
-      icon_model.save((err, item) => {
-        if (err) console.log(err)
+        assetModel
+          .findOneAndUpdate({ asset_id: asset_model.asset_id })
+          .then((response) => {
+            if (response === null) {
+              asset_model.save(function (err, item) {
+                if (err) return console.error(err);
+                console.log(item + ' saved to bookstore collection.');
+              });
+            }
+          });
       });
     })
+    .then(() => setTimeout(()=>get_assets(), millisecons()));
+};
+const get_icons = () => {
+  fetch(URL + icons_exchanges, {
+    method: 'GET',
+    headers: {
+      'X-CoinAPI-Key': api_key,
+    },
   })
-  .catch(err => console.log(err))
-}
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((icon) => {
+        const icon_model = new iconModel(icon);
+        iconModel
+          .findOneAndUpdate({ asset_id: icon_model.asset_id })
+          .then((response) => {
+            if (response === null) {
+              icon_model.save((err, item) => {
+                if (err) console.log(err);
+              });
+            }
+          });
+      });
+    })
+  fetch(URL + icons_assets, {
+    method: 'GET',
+    headers: {
+      'X-CoinAPI-Key': api_key,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((icon) => {
+        const icon_model = new iconModel(icon);
+        iconModel
+          .findOneAndUpdate({ asset_id: icon_model.asset_id })
+          .then((response) => {
+            if (response === null) {
+              icon_model.save((err, item) => {
+                if (err) console.log(err);
+              });
+            }
+          });
+      });
+    })
+    .then(() => setTimeout(()=>get_icons(), millisecons()));
+};
 app.use(cors());
-app.use(routes)
+app.use(routes);
 socket.connect(server);
-server.on('connection', socket => {
-  console.log(`user connected:`, socket );
-})
+server.on('connection', (socket) => {
+  console.log(`user connected:`);
+});
 server.listen(port, () => {
   // console.log('starting from docker sdfsdf');
   connect();
